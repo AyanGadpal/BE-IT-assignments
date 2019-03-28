a2a76eac701507cce251576191042385e763ed0a
Ayan Naresh Gadpal <36962224+Z-Erian@users.noreply.github.com>
2019-03-28 15:52:27 +0530

    Update 9hashing.cpp

--- SEM 2/Practical/DSFL/9hashing.cpp
+++ SEM 2/Practical/DSFL/9hashing.cpp
@@ -9,16 +9,16 @@
 #include <iomanip>
 #include <string.h>
 
-#define MAX 30
+#define MAX 10
 using namespace std;
 
 inline void line()
 {
-	cout << "\n=================================================\n";
+	cout << "\n======================================\n";
 }
 inline void sLine()
 {
-	cout << "\n------------------------------------------------";
+	cout << "\n--------------------------------------";
 }
 
 inline void errorMsg(const char *msg)
@@ -28,6 +28,13 @@ inline void errorMsg(const char *msg)
 		 << " ERROR : " << msg;
 	sLine();
 }
+inline void title(const char *msg)
+{
+	line();
+		cout<<msg;
+	line();
+}
+
 
 int hash(int tel)
 {
@@ -55,42 +62,127 @@ class HashTable
 {
 public:
 	Hashrow H[MAX];
-
+	int count;
 	HashTable()
 	{
-
+		count = 0;
 	}
-
+	bool isFull()
+	{
+		return count == MAX;
+	}
 	void display()
 	{
-		char *name;
-		cout<<setw(2)<<"SN"<<setw(20)<<"Name"<<setw(14)<<" Telephone\t\t"<<setw(3)<<"link";
-		for(int i = 0;i<30;i++)
+		cout<<setw(2)<<"SN"<<setw(15)<<"Name"<<setw(14)<<" Telephone	"<<setw(4)<<"link";
+		for(int i = 0;i<MAX;i++)
 		{
-			cout<<endl<<setw(2)<<i<<setw(20)<<H[i].name<<setw(10)<<H[i].tel<<"\t"<<setw(3)<<H[i].link;
+			cout<<endl<<setw(2)<<i<<setw(15)<<H[i].name<<setw(10)<<H[i].tel<<"\t"<<setw(3)<<H[i].link;
 		}
 	}
 	// Without Replacement
 	void CreaeHashTableNR()
 	{
+		int n;
+		sLine();
+		cout<< "\nNumber of Records : " ;
+		cin>>n;
+		for(int i =0;i<n;i++)
+		{
+			InsertNR();
+		}
+	}
+
+	void InsertNR()
+	{
 		char name[30];
-		int i,tel;
+		Hashrow temp;
+		int i,tel,n;
 		cout<<"\nEnter The Name : ";
 		cin>>name;
 		cout<<"\nEnter Telephone No. : ";
 		cin>>tel;
 		i = hash(tel);
-		strcpy(H[i].name,name);
-		H[i].tel = tel;
+		n = i;
+		if(!isFull())
+			while(1)
+			{
+				if(H[i].tel==0)
+				{
+					strcpy(H[i].name,name);
+					H[i].tel = tel;
+					count++;
+					return;
+				}
+				if(H[i].tel%MAX != n)
+				{
+					temp = H[i];
+					H[i].name = name;
+					H[i].tel = tel;
+				}
+				else
+				{
+					i = (i+1) % MAX;
+					H[n].link = i;
+				}
 
+
+			}
+		else
+			errorMsg("Hash Table Is Full!");
 	}
 
+	void Search()
+	{
+		int ph,i;
+		cout<<"\nEnter the Phone Number Of Person you want to Search\n";
+		cin>>ph;
+		i = hash(ph);
+
+		if(H[i].tel==0)
+		{
+			cout<<"\nRecord Not Present\n";
+			return;
+		}
+
+		do
+		{
+			if(H[i].tel == ph)
+			{
+				cout<<"Record found !! \n Name is : "<<H[i].name;
+				return;
+			}
+			i = H[i].link ;
+		}while(i != -1);
+
+		cout<<"\nRecord Is not Present !";
+	}
 	// With Replacement
 	void CreaeHashTableR()
 	{
-		cout<<"With Replacement";
+		int n;
+		sLine();
+		cout<< "\nNumber of Records : " ;
+		cin>>n;
+		for(int i =0;i<n;i++)
+			InsertR();
 	}
+	void InsertR()
+	{
+		char name[30];
+		int i,tel,n;
+		cout<<"\nEnter The Name : ";
+		cin>>name;
+		cout<<"\nEnter Telephone No. : ";
+		cin>>tel;
+		i = hash(tel);
+		n = i;
+		if(!isFull())
 
+		else
+			errorMsg("Hash Table Is Full!");
+
+
+	}
 };
 
 
@@ -100,7 +192,7 @@ int main() {
 	int ch=0;
 	bool hChoice;
 	HashTable H;
-	cout<<hash(45);
+	cout<<hash(2);
 
 	while(ch!=3)
 	{
@@ -116,52 +208,33 @@ int main() {
 		while(ch != 6)
 		{
 			line();
-			cout<< (hChoice ? "ALGO : With Replacement\n" : " ALGO : Without Replacement");
+			title(hChoice ? "ALGO : With Replacement Menu\n" : " ALGO : Without Replacement Menu");
 			cout << "\nEnter Your Choice\n"
-					 	"1) Create Hashtable \n"
-						"2) Insert \n"
-						"3) Create \n"
-						"4) Search \n"
-						"5) Display \n"
-						"6) Exit \n"
+					 	"1) Insert Record/s \n"
+						"2) Search \n"
+						"3) Display \n"
+						"4) Exit \n"
 					"YOUR CHOICE : ";
 			cin >> ch;
 			line();
 			switch (ch)
-					{
+			{
 					case 1:
 						line();
 						hChoice ? H.CreaeHashTableR() : H.CreaeHashTableNR();
 						break;
+
 					case 2:
 						line();
-
-
+					    H.Search();
 						break;
+
 					case 3:
-						line();
-
-						break;
-					case 4:
-						line();
-
-
-						break;
-					case 5:
 						line();
 						H.display();
 						break;
-					case 9:
-						line();
-						cout << "\n\t\tGOOD BYE\n";
-						line();
-						break;
 			}
-
 		}
-
 	}
-
-
 	return 0;
 }
