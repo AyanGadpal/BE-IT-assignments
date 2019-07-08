40bc4cf9a602a5203dfc19ce499420cb5508b8ce
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2019-07-08 15:51:27 +0530

    Database Exported

---
+++ TE/Sem 1/SL1 Database/orderManagment.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
+--
+-- Host: localhost    Database: orderManagment
+-- ------------------------------------------------------
+-- Server version	5.7.26-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `C_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `address` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`C_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Ayan','9881522447','ayan@gmail.com','Nagpur'),(2,'Kalpit.c',NULL,NULL,NULL),(3,'Archana','8999331239','archi@gmail.com','Nagpur'),(4,'Zhahil',NULL,NULL,NULL),(5,'Manasi','7757025466','manasi@gmail.com','pune'),(6,'dnyanu','9887372321','dnanu@gmail.com','Nagpur'),(7,'Atharva',NULL,'Ath@gmail.com','Nagpur'),(8,'Gavruav','9876777767','Gav@gmail.com','pune'),(9,'Ankit','9087886753','ank@gmail.com','pune'),(10,'Manas',NULL,'man@gmail.com','Goa');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_record`
+--
+
+DROP TABLE IF EXISTS `order_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_record` (
+  `O_ID` int(11) NOT NULL,
+  `DOP` datetime NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total_cost` float NOT NULL,
+  `P_ID` int(11) NOT NULL,
+  `C_ID` int(11) NOT NULL,
+  PRIMARY KEY (`O_ID`,`P_ID`,`C_ID`),
+  KEY `C_ID` (`C_ID`),
+  KEY `P_ID` (`P_ID`),
+  CONSTRAINT `order_record_ibfk_1` FOREIGN KEY (`C_ID`) REFERENCES `customer` (`C_ID`),
+  CONSTRAINT `order_record_ibfk_2` FOREIGN KEY (`P_ID`) REFERENCES `product` (`P_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_record`
+--
+
+LOCK TABLES `order_record` WRITE;
+/*!40000 ALTER TABLE `order_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `P_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `P_name` varchar(20) NOT NULL,
+  `P_price` float NOT NULL,
+  `P_stock` int(11) NOT NULL,
+  PRIMARY KEY (`P_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Asus-Laptop',91000,4),(2,'Dell',37000,4),(3,'Sony',47000,2),(4,'Acer',33000,10),(5,'Apple',137000,2);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-08 15:48:19
