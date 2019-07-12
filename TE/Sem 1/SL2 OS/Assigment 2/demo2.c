52d350ede4e86bd3c364f6e113aac936aee67571
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2019-07-12 10:57:39 +0530

    Update demo2.c

--- TE/Sem 1/SL2 OS/Assigment 2/demo2.c
+++ TE/Sem 1/SL2 OS/Assigment 2/demo2.c
@@ -2,12 +2,57 @@
 #include<unistd.h>
 #include<stdlib.h>
 
+void binarySearch(int n,int array[],int key)
+{
+    int i=0,fs,ls,m;
+    fs= 0;
+    ls = n-1;
+    m = (fs+ls)/2;
+    while(1)
+    {
+       
+        i++;
+        if(i==10)
+         break;
+        if(key == array[m])
+        {
+            printf("Found at position %d!\n",m);
+            break;
+        }
+        else if(key < array[m])
+        {
+            if(ls==m)
+            {
+                printf("\nNOT FOUND !\n");
+                break;
+            }
+            ls = m;
+            m = (fs+ls)/2;
+        }
+        else if(key > array[m])
+        {
+            if(m==fs)
+            {
+                printf("\nNOT FOUND !\n");
+                break;
+            }
+            fs = m;
+            m = (fs+ls)/2;
+        }
+    }      
+
+}
+
 int main(int argc, char *argv[])
 {
-    int i;
+    int i,key,*array;
+    array = (int *) malloc(sizeof(int)*argc);
+    printf("\nEnter the element you want to search : ");
+    scanf("%d",&key);
     for(i=0;i<argc;i++)
     {
-        printf("%c ",*argv[i]);
+        *(array+i) = atoi(argv[i]);
     }
+    binarySearch(argc,array,key);
     return(0);
 }
