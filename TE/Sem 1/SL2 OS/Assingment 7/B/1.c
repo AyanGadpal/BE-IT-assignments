611c0b987b0b6871e076d73d3a6cc4bd769659a6
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2019-09-13 09:50:28 +0530

    Minor changes in 7 Part B

--- TE/Sem 1/SL2 OS/Assingment 7/B/1.c
+++ TE/Sem 1/SL2 OS/Assingment 7/B/1.c
@@ -1,9 +1,4 @@
-/*====================================================
-Auther : Ayan N Gadpal K11 33308
-Code : Matrix Multiplication using multithreading
-Date : Sep 6 2019
-Copyright : GNU public Library
-=====================================================*/
+
 
 // NOTE : THIS IS THE FIRST PROCESS
 
@@ -21,14 +16,17 @@ int main()
     int fd, ch;
 
     char *filename = "/tmp/contain.txt", str[MAX];
-    char *stats = "/tmp/stats.txt";
+    char *stats = "/tmp/contain2.txt";
     mkfifo(filename, 0666);
-    mkfifo(stats, 0666);
+      mkfifo(stats, 0666);
     fd = open(filename, 0666);
     printf("\nPlease Enter the Data you want to on the file\n");
     scanf("%[^\t]s", str);
     write(fd, str, strlen(str) + 1);
-    int fd2 = open(stats, O_RDONLY);
+    int fd2 = open(stats, O_RDONLY); 
+    // read(fd2, str, MAX);
+    // printf("\nREAD : \n %s",str);
+    close(fd2);
     close(fd);
     return 0;
 }
