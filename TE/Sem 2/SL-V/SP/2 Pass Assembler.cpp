61ec6de4cbc83cd934b23ee0ae629a03056ea224
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2020-01-10 14:59:31 +0530

    Started 2nd Pass of Assembler

--- TE/Sem 2/SL-V/SP/2 Pass Assembler.cpp
+++ TE/Sem 2/SL-V/SP/2 Pass Assembler.cpp
@@ -1,19 +1,19 @@
 //=================================================================================
-// Name        : 2 SL-V Assembler
+// Name        : 3 SL-V Assembler's 2 Pass Output
 // Author      : Ayan Gadpal 33308
 // Version     : 1.0
 // Copyright   : GNU Public License
-// Date 	   : 20 Dec 2019
-// Description : Assembler
-// Note you will input.txt and MachineCode.txt
+// Date        : 10 Jan 2020
+// Description : 2 Pass Assembler
+// Status      : 0% Done 
+//
+// TODOs	
 //=================================================================================
-
-#include <iostream>
-#include <fstream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
+// Basic Row used by Various Classes 
 struct Row
 {
     string symbol;
@@ -21,124 +21,154 @@ struct Row
     int value;
 };
 
-class TranslationTable
+class pool
 {
 private:
-    struct Row row[20]; 
-    int len; // length of element present
-    int LC;
-
+   int row[20];
+   int top; 
 public:
-    // Initialize the Table
-    TranslationTable()
-    {
-        row[0].symbol = "START";
-        row[0].type = "AD";
-        row[0].value = 01;
+};
 
-        row[1].symbol = "END";
-        row[1].type = "AD";
-        row[1].value = 02;
+class litralTable
+{
+private:
 
-        row[2].symbol = "MOVER";
-        row[2].type = "IS";
-        row[2].value = 01;
-        
-        row[3].symbol = "ADD";
-        row[3].type = "IS";
-        row[3].value = 02;
+  struct Row row[20];
+  int top;
+public:
 
-        row[4].symbol = "AREG";
-        row[4].type = "R";
-        row[4].value = 1;
-
-        row[5].symbol = "BREG";
-        row[5].type = "R";
-        row[5].value = 2;
-
-        len = 6;
-    }
-
-    // Match the symbols to the opcode
-    int match(const char *symbol)
-    {
-        // iterator
-        int i;
-
-        // For each entry in table // Consider using hash table for such case to speed up
-        for (i = 0; i < len; i++)
-            if (strcmp(symbol, row[i].symbol.c_str()) == 0) // Compare with Each symbol
-                return i;
-
-        return -1; // Not Found
-    }
-
-    // Translate the ASM ( taken "txt" for simplicity ) file into Machine Code
-    void Translate(const char *inputFilename)
-    {
-        ifstream in; // Input file
-        ofstream out; // Output file
-
-        // Open the file
-        in.open(inputFilename);
-
-        // Open the Destination file
-        out.open("MachineCode.txt");
-
-        string str; 
-        
+  litralTable()
+  {
+	top = 0;
+  }
+   
+  void readFromFile()
+  {
+		ifstream in;
+		in.open("SymbolTable.txt");
+		string str; 
+        int index = 0;
         // Process line by line input
         while (getline(in, str))
         {
-            // Covert string into char[]
+        	 // Covert string into char[]
             int n = str.length();
             char chstr[n + 1];
+           
             strcpy(chstr, str.c_str());
 
-            // divide by space
-            char *token = strtok(chstr, " ");
-
-            // Check whether label
-            if (match(token) != -1)
-                out << "_";
-
-            // For each word in the sentence
-            while (token != NULL)
-            {
-                // Find Opcode
-                int id = match(token);
-
-                // Got Opcode
-                if (id != -1)
-                {
-                    out << " (" << row[id].type << ","<<row[id].value<<") ";
-                }
-                // No Opcode, i.e unidentified symbol or label
-                else
-                    out << " " << token;
-                
-                // Go to next word
-                token = strtok(NULL, " ");
-            }
-            out << endl;
+            // Splite Word by space
+            char *token = strtok(chstr, " "); // Index
+            
+            token = strtok(NULL, " "); // Symbol
+           
+            row[index].symbol = token;
+             	
+            token = strtok(NULL, " "); // Address   
+             
+            row[index].value = atoi(token); 
+            index++; 
+            top = index;
         }
-
-        // Close the Files
+        
         in.close();
-        out.close();
-    }
+	}
+	void display()
+	{
+		cout<<"SYMBOL TABLE \n";
+		for(int i =0;i<top;i++)
+			cout<<endl<<i<<" "<<row[i].symbol<<" "<<row[i].value;
+	}
+
+};
+// Keep track of Symbols
+class symbolTable
+{
+
+private:
+   struct Row row[20];
+   int top; 
+   
+public:
+
+	symbolTable()
+	{
+		top = 0;
+	}
+	
+	
+	void readFromFile()
+	{
+		ifstream in;
+		in.open("SymbolTable.txt");
+		string str; 
+        int index = 0;
+        // Process line by line input
+        while (getline(in, str))
+        {
+        	 // Covert string into char[]
+            int n = str.length();
+            char chstr[n + 1];
+           
+            strcpy(chstr, str.c_str());
+
+            // Splite Word by space
+            char *token = strtok(chstr, " "); // Index
+            
+            token = strtok(NULL, " "); // Symbol
+           
+            row[index].symbol = token;
+             	
+            token = strtok(NULL, " "); // Address   
+             
+            row[index].value = atoi(token); 
+            index++; 
+            top = index;
+        }
+        
+        in.close();
+	}
+	void display()
+	{
+		cout<<"SYMBOL TABLE \n";
+		for(int i =0;i<top;i++)
+			cout<<endl<<i<<" "<<row[i].symbol<<" "<<row[i].value;
+	}
+
 };
 
-// Driver Code
-int main()
+struct BinaryRow
 {
-    // Create the table
-    TranslationTable T;
-
-    // Call the translator
-    T.Translate("input.txt");
-
-    return 0;
+	int Address;
+	int opcode;
+	int reg;
+	int memAddress;
+	BinaryRow()
+	{
+		Address = opcode = reg = memAddress = 0;
+	}
+};
+class MachineCode
+{
+private:
+	struct BinaryRow Row[20];
+	int len;
+public:
+	MachineCode()
+	{
+		len = 0;
+	}
+	
+	// Create Translation Method Here
 }
 
-// End of code
+
+int main()
+{
+	symbolTable ST;
+	
+    ST.readFromFile();
+	ST.display();
+	return 0;
+}
+
