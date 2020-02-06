79830f2d77b697b67aa2440ca7c518d4848dde4a
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2020-02-06 09:08:38 +0530

    Completed with Comments

--- TE/Sem 2/SL-V/DAA/optimalTape.cpp
+++ TE/Sem 2/SL-V/DAA/optimalTape.cpp
@@ -1,24 +1,34 @@
 //=================================================================================
 // Name        : A3 SL-V DAA Optimal Storage
 // Author      : Ayan Gadpal 33308
-// Version     : 2.0
-// Date        : 05 Feb 2020
+// Version     : 3.0
+// Date        : 06 Feb 2020
 // Description : Use Greedy Approach to optimal storage on tape
 //=================================================================================
 #include <iostream>
 #include <bits/stdc++.h>
+
+// Macro To Print a line
 #define line() (cout<<"\n================================")
 using namespace std;
 
-
+// Disk which contain a single Tape and keep Track of it
 struct Disk{
+    
+    // "Limit" : is the total Storage of tape
+    // "Load" : Total storage used
+    // "top" : Keep track of topmost empty elements
     int limit,load,top;
+    
+    // Tape : Storage Tape 
     int *Tape;
 
+    // Intialize the top and load to 0
     Disk(){
         top = load = 0;
     }
 
+    // Display the contain on the tape along with its Total Storage used and return its Avg Retrieval time
     float display()
     {
         for(int i=0;i<top;i++)
@@ -27,11 +37,17 @@ struct Disk{
         return (float)load/top;
     }
 
+    // Assign Total Elemets to the Tape
+    // In Greedy Approach if we will need to store max of (TotalJobs/TotalTape + 1) in a Tape
     void AssignMemory(int limit,int elementNumber)
     {
         this->limit = limit;
         Tape = new int[elementNumber];
     }
+
+    // Push the element on list
+    // if tape is not overloaded then return true
+    // else return flase
     bool push(int element)
     {
         // Check for Tape Overflow
@@ -41,28 +57,45 @@ struct Disk{
 
         // Add Element on the tape
         Tape[top] = element;
+
+        // Increase the load
         load+=element;
+        
+        // Point to next empty slot
         top++;
         
         return true;
     }
 };
-class Storage{
+
+
+// Used For Storing the Jobs on Multiple Tapes 
+ class Storage{
     private:
+        // 'n' : Number of Jobs
+        // 'm' : Size of Each Tape ( Cosidered to be same of all tape )
+        // 'tapes' : Total number of Tapes
         int n,m,tapes;
+
+        // JOB LIST store the jobs with their weight
         int *jobList;
+
+        // Array of Object "Disk", Each Disk will keep track of 1 Tape
         struct Disk *T;
     public:
+
+        // Intialize the Storage 
         Storage()
         {
+            // Accept the Total Number of Jobs
             cout<<"\nEnter The number of jobs : ";
             cin>>n;
             line();
 
+            // Accept the Size for Each job, Individually
             cout<<"\nEnter The size of each job";
             jobList = new int[n];
             line();
-
             for(int i = 0;i<n;i++)
             {
                 cout<<"\nSize of JOB NO."<<i+1<<" : ";
@@ -70,10 +103,12 @@ class Storage{
             }
             line();
 
+            // Accept the Total Number of tapes
             cout<<"\nEnter the number of Tape : ";
             cin>>tapes;
             line();
 
+            // Accept the Size of tape
             cout<<"\nEnter the size of Tape : ";
             cin>>m;
             line();
@@ -83,24 +118,31 @@ class Storage{
             for(int i=0;i<tapes;i++)
                 T[i].AssignMemory(m,(n/tapes)+1);
 
-            // Merge Sort
+            // Merge Sort the Joblist in Ascending order
             mergeSort(jobList,0,n-1);
         }
+
+        // Display the JobList
         void displayJobs()
         {
             for(int i=0;i<n;i++)
                 cout<<"\n"<<i+1<<") "<<jobList[i];   
         }
 
+        // Display the Contain of all tapes along with total Average Retrival time
         void displayTapes()
         {
             float totalAvg = 0;
+            
+            // Contain of all Tapes
             for(int i=0;i<tapes;i++)
                 line(),cout<<"\nTape "<<i+1<<endl,totalAvg += T[i].display();  
 
+            // Total Avg Time
             cout<<"\nTotal Average is "<<totalAvg/tapes<<endl; 
             line();
         }
+
 
         void merge(int arr[], int l, int m, int r)
         {
@@ -171,10 +213,15 @@ class Storage{
 
 };
 
+// Driver Code
 int main()
 {
+    // Create an Storage Intance, 
     Storage HDD;
+
+    // Call the Sub modules
     HDD.placeOnTape();
     HDD.displayTapes();
     return 0;
 }
+
