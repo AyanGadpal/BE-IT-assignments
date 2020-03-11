d4eb84867a296f07f533838b0024bcdc43e93570
Ayan Naresh Gadpal <36962224+AyanGadpal@users.noreply.github.com>
2020-03-11 15:01:04 +0530

    Update Lex.cpp

--- TE/Sem 2/SL-V/DAA/Lex.cpp
+++ TE/Sem 2/SL-V/DAA/Lex.cpp
@@ -6,7 +6,7 @@
 // Modified    : 26 Feb 2020
 // Total Time  : 2 Hours
 // Description : 1st Pass of 2 Pass Assembler
-// Status      : 22% Done
+// Status      : 92% Done
 //
 // Note you will need tu put your code in "program.c"
 //
@@ -19,9 +19,15 @@
 #include <bits/stdc++.h>
 #define MAX 20
 using namespace std;
+
+struct Row
+{
+    string name;
+};
+
 class Table
 {
-private:
+protected:
     string row[20];
     int Top;
 public:
@@ -40,17 +46,48 @@ public:
             cout<<"\n"<<row[i];
     }
 };
+
+class UniformSymbolTable : public Table
+{
+    protected:
+        char class[4];
+    public:
+
+    void add(const char *row)
+    {
+        this->row[Top] = row;
+        Top++;
+    }
+    // CONDING HERE <-----------------------------------
+}
+
 class Lexical
 {
 private:
+    int size;
+    Row TerminalTAB[28];
 
 public:
+    Lexical()
+    {
+        size=28;
+        ifstream in;
+        in.open("TerminalTable");
+        string str;
+        int i=0;
+        while (getline(in, str))
+        {
+           TerminalTAB[i].name = str;
+           i++;
+        }
+    }
     int category(char S)
     {
         // NUMERIC TYPE
         if(S >= 48 && S<=57)
             return 1;
-
+        if(S == 34)//"
+            return 2;
         // Name 
         else if((S>=65 && S<=90)||(S>=97 && S<=122))
             return 0;
@@ -59,10 +96,21 @@ public:
         return -1;
     }
 
+    int checkTerminal(const char* symbol)
+    {
+        for(int i=0;i<size;i++)
+        {
+            if(strcmp(symbol,TerminalTAB[i].name.c_str())==0)
+            return i;
+        }
+        return -1;
+
+    }
+
     void Analyze(const char *inputFilename)
     {
         ifstream in;
-        Table LitralTable,IdentiferTable;
+        Table LitralTable,IdentiferTable,;
         in.open(inputFilename);
         string str;
         while (getline(in, str))
@@ -72,15 +120,31 @@ public:
             char chstr[n + 1];
             int opcodeSize = 1;
             strcpy(chstr, str.c_str());
+
             int len = str.length();
             int first = 0;
 
             for(int i=0;i<len && i<MAX;i++)
             {
                 int C = category(chstr[i]);
-                if(C == 1)
+                // Litral By Opening double inverted comma
+                if(C == 2)
                 {
-                    cout<<endl<<chstr[i]<<" "<<"NUMBER";
+                    char inputBuffer[MAX];
+
+                    i++;
+
+                    for(int j=0;j<MAX && category(chstr[i])!= 2;j++,i++)
+                        inputBuffer[j] = chstr[i];
+                    inputBuffer[j]='\0';
+
+                    LitralTable.add(inputBuffer);
+                
+                }
+                // Litral By 1st char is a number
+                else if(C == 1)
+                {
+                    
                     char inputBuffer[MAX];
                     int j=0;
                     for(j=0;j<MAX && category(chstr[i])== 1;j++,i++)
@@ -90,22 +154,25 @@ public:
                     inputBuffer[j]='\0';
                     LitralTable.add(inputBuffer);
                 }
+
+                // Identifier or Terminal since it's a word
                 else if(C == 0)
                 {
-                    cout<<endl<<chstr[i]<<" "<<"CHAR";
                     char inputBuffer[MAX];
-                    int j;
-                    for(j=0;j<MAX && isalnum(chstr[i]);j++,i++)
-                    {
+
+                    for(int j=0;j<MAX && isalnum(chstr[i]);j++,i++)
                         inputBuffer[j] = chstr[i];
-                    }
                     inputBuffer[j]='\0';
-                    IdentiferTable.add(inputBuffer);
+
+                    // Check if the word is an terminal in Terminal table
+                    int Tid = checkTerminal(inputBuffer);
+
+                    if(Tid == -1)
+                        IdentiferTable.add(inputBuffer);
+                    else
+                        cout<<endl<<inputBuffer<<" "<<"TERMINAL";
                 }
-                else
-                {
-                    cout<<endl<<chstr[i]<<" "<<"SYMBOL";
-                }
+              
                 
             }
             
