0bec48da1e6e0649162a1514e42cadf76fe9a245
Ayan Naresh Gadpal <ayangadpal2@gmail.com>
2020-08-11 10:26:17 +0530

    CLVII RSA Commented

--- BE/SEM 1/CL-VII/Assigment 1 RSA.cpp
+++ BE/SEM 1/CL-VII/Assigment 1 RSA.cpp
@@ -7,6 +7,8 @@
 #include<string.h>
 using namespace std;
 
+
+// Calculate the GCD
 int gcd(int a, int b)
 {
     while (b)
@@ -18,6 +20,7 @@ int gcd(int a, int b)
     return a;
 }
 
+// Get the 'e' 
 double getE(double phi)
 {
     int flag =0;
@@ -33,25 +36,28 @@ double getE(double phi)
 }
 
 
-
+// Driver code
 int main()
 {
-    double p,q,n,d,e,phi,flag;
+    // RSA Variable
+    double p,q,n,d,e,phi;
     
     // Currently take a static p and q
     p = 19;
     q = 61;    
-    n = p*q;
-    phi = (p-1)*(q-1);
+    n = p*q; // Calculate n
+    phi = (p-1)*(q-1); // Total number of co prime of 'n'
 
-    e = getE(phi);
+    e = getE(phi); // Get e (encryption key) such that, 1 < e < phi(n) AND e and phi n are co prime 
 
-    d=fmod((1/e),phi);
+    d=fmod((1/e),phi); // Decryption key
  
     double message = 4;
-    double c1 = pow(message,e); //encrypt the message
+
+    // Encryption and Decryption of message
+    double c1 = pow(message,e);
     double m = pow(c1,d); 
-    double c=fmod(c1,n);
+    double c = fmod(c1,n);
     m=fmod(m,n);
 
     cout<<"p: "<<p<<"\n";
@@ -60,8 +66,9 @@ int main()
     cout<<"phi: "<<phi<<"\n";
     cout<<"e: "<<e<<"\n";
     cout<<"Original Message: "<<message<<"\n";
-    cout<<"Decrypted Message: "<<m<<"\n";
     cout<<"Encrypted Message: "<<c<<"\n";
+    cout<<"Decrypted Message: "<<m<<"\n";
+
 
     
     // cout<<generate_coprime(n,phi);
